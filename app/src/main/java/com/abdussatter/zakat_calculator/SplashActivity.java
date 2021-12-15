77cd8c9b2336ff5ab10fca18bb760adff073f1a1
Abdus Satter <abdussatter51@gmail.com>
2021-12-15 21:48:56 +0600

    Add Splash Screen and Apps Logo

--- app/src/main/java/com/abdussatter/zakat_calculator/SplashActivity.java
+++ app/src/main/java/com/abdussatter/zakat_calculator/SplashActivity.java
@@ -2,13 +2,36 @@ package com.abdussatter.zakat_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,
+                        MainActivity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, 5000);
+
+
+
     }
 }
